946d4fc526f95adc90ad977d2a9f9b7d7eeb1e2a
Hemika Yasinda Kodikara <hemika.kodikara@gmail.com>
2015-03-18 16:30:24 +0530

    Added shaded jar implementation

---
+++ src/main/java/org/wso2/mb/failover/Main.java
@@ -0,0 +1,78 @@
+package org.wso2.mb.failover;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.SubnodeConfiguration;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.mb.integration.common.clients.AndesClient;
+import org.wso2.mb.integration.common.clients.configurations.AndesJMSConsumerClientConfiguration;
+import org.wso2.mb.integration.common.clients.configurations.AndesJMSPublisherClientConfiguration;
+import org.wso2.mb.integration.common.clients.exceptions.AndesClientConfigurationException;
+import org.wso2.mb.integration.common.clients.exceptions.AndesClientException;
+import org.wso2.mb.integration.common.clients.operations.utils.AndesClientConstants;
+import org.wso2.mb.integration.common.clients.operations.utils.AndesClientUtils;
+
+import javax.jms.JMSException;
+import javax.naming.NamingException;
+import java.io.IOException;
+
+/**
+ *
+ */
+public class Main {
+    public static void main(String[] args)
+            throws ConfigurationException, JMSException, NamingException, AndesClientConfigurationException, AndesClientException, IOException {
+        Logger log = LoggerFactory.getLogger(Main.class);
+        String xmlFilePath = args[0];
+        log.info("Config File = " + xmlFilePath);
+        XMLConfiguration config = new XMLConfiguration(xmlFilePath);
+        AndesClient consumerClient = null;
+        if(0 < config.configurationsAt("base.consumer").size()){
+            SubnodeConfiguration consumerNode = config.configurationAt("base.consumer");
+            int count = consumerNode.getInt("[@count]", 1);
+
+            log.info("Creating consumer(s)...");
+            AndesJMSConsumerClientConfiguration consumerConfig = new AndesJMSConsumerClientConfiguration(xmlFilePath);
+
+            if (log.isDebugEnabled()) {
+                log.info(consumerConfig.toString());
+            }
+
+            consumerClient = new AndesClient(consumerConfig, count, true);
+            consumerClient.startClient();
+        }else{
+            log.info("No consumers are created");
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        AndesClient publisherClient;
+        if(0 < config.configurationsAt("base.publisher").size()){
+            SubnodeConfiguration publisherNode = config.configurationAt("base.publisher");
+            int count = publisherNode.getInt("[@count]", 1);
+
+            log.info("Creating publisher(s)...");
+            AndesJMSPublisherClientConfiguration publisherConfig = new AndesJMSPublisherClientConfiguration(xmlFilePath);
+
+            if (log.isDebugEnabled()) {
+                log.info(publisherConfig.toString());
+            }
+
+            publisherClient = new AndesClient(publisherConfig, count, true);
+            publisherClient.startClient();
+        }else{
+            log.info("No publishers are created");
+        }
+
+        log.info("Clients created...");
+
+        if(null != consumerClient){
+            AndesClientUtils.waitForMessagesAndShutdown(consumerClient, AndesClientConstants.DEFAULT_RUN_TIME);
+        }
+    }
+}
